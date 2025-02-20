6dd85ea398967ad26d157d969d498dbf77a4abc2
sumanthmx <70922037+sumanthmx@users.noreply.github.com>
2025-02-20 03:27:00 -0500

    remove video and file upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import VideoUpload from './components/VideoUpload';
 import LLMSearchBar from './components/VideoSearch';
 
 export default function Home() {
   return (
     <div>
       <h1>Video Upload and LLM Search</h1>
-      <VideoUpload />
       <LLMSearchBar />
     </div>
   );
